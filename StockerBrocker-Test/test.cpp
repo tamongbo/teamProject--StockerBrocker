ca15b20ac643acf5b126833774ea57ca8dfdae3c
wonpyo.han <olarya@naver.com>
2024-05-21 15:55:26 +0900

    add buyNiceTiming feature

--- StockerBrocker-Test/test.cpp
+++ StockerBrocker-Test/test.cpp
@@ -8,7 +8,10 @@
 using namespace std;
 using namespace testing;
 
-class AdapterMock : public Adapter {
+int MAX_BUY_COUNT = 100;
+
+class AdapterMock : public Adapter
+{
 public:
 	MOCK_METHOD(void, login, (string, string), (override));
 	MOCK_METHOD(void, buy, (string, int, int), (override));
@@ -16,33 +19,98 @@ public:
 	MOCK_METHOD(int, currentPrice, (string, int), (override));
 };
 
-TEST(TestCaseName, LoginSuccess) {
+TEST(TestCaseName, LoginSuccess)
+{
 	StockerBrocker app;
 	AdapterMock mock;
-	
+
 	app.selectStockBrocker(&mock);
 
 	string name = "Fake Name";
 	string password = "Fake Password";
-	
+
 	EXPECT_CALL(mock, login(name, password))
 		.Times(1);
 
 	app.login(name, password);
 }
 
-TEST(TestCaseName, BuySuccess) {
+TEST(TestCaseName, BuySuccess)
+{
 	StockerBrocker app;
 	AdapterMock mock;
 
 	app.selectStockBrocker(&mock);
 
 	string stockCode = "0xFF";
-	int count = 0; 
+	int count = 0;
 	int price = 0;
 
 	EXPECT_CALL(mock, buy(stockCode, count, price))
 		.Times(1);
 
 	app.buy(stockCode, count, price);
+}
+
+TEST(TestCaseName, getPriceSuccess)
+{
+	StockerBrocker app;
+	AdapterMock mock;
+
+	app.selectStockBrocker(&mock);
+
+	string stockCode = "0xFF";
+	int count = 0;
+	int price = 0;
+
+	EXPECT_CALL(mock, currentPrice(stockCode, 1))
+		.Times(1);
+
+	app.currentPrice(stockCode, 1);
+
+	EXPECT_CALL(mock, currentPrice(stockCode, 1))
+		.Times(1);
+
+	app.currentPrice(stockCode, 1);
+}
+
+TEST(TestCaseName, BuyNiceTimingWhenBuyCase)
+{
+	StockerBrocker app;
+	AdapterMock mock;
+
+	app.selectStockBrocker(&mock);
+
+	string stockCode = "0xFF";
+	int count = 0;
+	int price = 0;
+
+	EXPECT_CALL(mock, currentPrice(stockCode,0)).
+		WillOnce(Return(100)).
+		WillOnce(Return(120)).
+		WillOnce(Return(140));
+
+	EXPECT_CALL(mock, buy(stockCode, MAX_BUY_COUNT, price)).Times(1);
+
+	app.buyNiceTiming(stockCode, price);
+}
+TEST(TestCaseName, BuyNiceTimingWhenNotBuyCase)
+{
+	StockerBrocker app;
+	AdapterMock mock;
+
+	app.selectStockBrocker(&mock);
+
+	string stockCode = "0xFF";
+	int count = 0;
+	int price = 0;
+
+	EXPECT_CALL(mock, currentPrice(stockCode, 0)).
+		WillOnce(Return(100)).
+		WillOnce(Return(180)).
+		WillOnce(Return(140));
+
+	EXPECT_CALL(mock, buy(stockCode, MAX_BUY_COUNT, price)).Times(0);
+	app.buyNiceTiming(stockCode, price);
+
 }
--- StockerBrocker/StockerBrockerApp.cpp
+++ StockerBrocker/StockerBrockerApp.cpp
@@ -1,28 +1,58 @@
 #pragma once
 #include "Adapter.cpp"
 
-class StockerBrocker {
+class StockerBrocker
+{
 public:
-	void selectStockBrocker(Adapter* adapter) {
+	void selectStockBrocker(Adapter* adapter)
+	{
 		this->adapter = adapter;
 	}
 
-	void login(string ID, string password) {
+	void login(string ID, string password)
+	{
 		adapter->login(ID, password);
 	}
 
-	void buy(string stockCode, int count, int price) {
+	void buy(string stockCode, int count, int price)
+	{
 		adapter->buy(stockCode, count, price);
 	}
 
-	void sell(string stockCode, int count, int price) {
+	void sell(string stockCode, int count, int price)
+	{
 		adapter->sell(stockCode, count, price);
 	}
 
-	int currentPrice(string stockCode, int minutes) {
-		adapter->currentPrice(stockCode, minutes);
+	int currentPrice(string stockCode, int minutes)
+	{
+		return adapter->currentPrice(stockCode, minutes);
+	}
+	void buyNiceTiming(string stockCode, int price)
+	{
+		if (isPriceIncrease(stockCode, price) == true)
+			adapter->buy(stockCode, MAX_BUY_COUNT, price);
+	}
+	bool isPriceIncrease(string stockCode, int price)
+	{
+		int priceCheckPriod = 10; //ms
+		const int MAX_CHECK_COUNT = 3;
+		int priceHistory[MAX_CHECK_COUNT + 1] = { 0 };
+		
+		for (int i = 1; i <= 3; i++)
+		{
+			priceHistory[i] = adapter->currentPrice(stockCode, 0);
+			Sleep(priceCheckPriod);
+			if (priceHistory[i - 1] >= priceHistory[i])
+			{
+				std::cout << priceHistory[i-1] << priceHistory[i] << std::endl;
+				return false;
+			}
+		}
+		return true;
 	}
 
 private:
 	Adapter* adapter;
+	int MAX_BUY_COUNT = 100;
 };
